5cd654f5a117bbf98798fa0033b7e05338f6ec5a
AndrewCowan2024 <AndrewCowan2024>
2021-09-21 09:11:08 -0500

    2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #WRITE YOUR CODE IN THIS FILE
 
 def topFiveMovies(a,b,c,d,e):
-    print("Spider-Man-1", "SpiderMan 2","SpiderMan 3","The Amazing Spiderman","The Amazing Spiderman 2")
+    print("a","b","c","d","e")
 
